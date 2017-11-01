5c5ca2bd0566db1f155448d1fd481c4aee33fda1
Jeroen Peeters <jeroen.peeters@evance.be>
2017-11-01 18:50:35 +0100

    Make async and ls through wt-cli API

--- commands/profile.js
+++ commands/profile.js
@@ -12,15 +12,15 @@ module.exports = function(program) {
     .option('-m, --migrate <oldprofile>', 'Migrate from this profile to a new profile ' + chalk.red.bold('THIS REMOVES THE OLD PROFILE!'))
     .option('-t --show-token', 'Show the Webtask token when listing all profiles')
     .option('-d --show-default', 'Show the default Webtask profile as well')
-    .action(function (cmd, p) {
+    .action(async function (cmd, p) {
       /**
        * TODO: delve deeper into wt-cli API!
+       * const configPath = wt.configFile().configPath;
+       * const cf = JSON.parse(fs.readFileSync(configPath)); // Config file
        */
 
       // Initialize here, use later.
       const c = cmd.toLowerCase(); // Command
-      const configPath = wt.configFile().configPath;
-      const cf = JSON.parse(fs.readFileSync(configPath)); // Config file
       const init = chalk.blue.bold('Wittier profile management');
 
       if (c === 'a' || c === 'add'){
@@ -67,8 +67,6 @@ module.exports = function(program) {
          * Note: this is a function created for completionist's sake.
          * If you're working with wittier to manage your profiles, it means you want a promptful approach to configuring Webtask profiles.
          * Thus, why not offer an ls command to list all existing profiles in your wt config file?
-         * This did mean, however, that we needed to access said file instead of just piping `wt profile ls (--show-token)`
-         * since it renders a non-coloured output.
          *
          * Part of our opinionated cli this the abandon of default profiles for a structure similar to folders.
          * This is why default does NOT get printed by default.
@@ -100,10 +98,13 @@ module.exports = function(program) {
 
         console.log(chalk.bold('[WITTIER] ') + 'Listing all wt-cli profiles:\n');
 
+        // Get profile list
+        const cf = await wt.configFile().load();
         const keys = Object.keys(cf);
+
         if (keys.length > 0){
           keys.forEach(key => {
-            if (key !== 'default'){
+            if (key !== 'default' || (key === 'default' && program.commands[0].showDefault)){
               console.log(chalk.blue('Profile:     ') + chalk.green(key));
               console.log(chalk.blue('URL:         ') + cf[key].url);
               console.log(chalk.blue('Container:   ') + cf[key].container);
@@ -114,7 +115,7 @@ module.exports = function(program) {
               );
             }
           });
-        }
+        } else console.log(chalk.red('No profiles found.'));
 
         process.exit();
       }
